14f58904da6f0b9129bb8becbfcf1ef536fbe026
gvrohan11 <71199424+gvrohan11@users.noreply.github.com>
2021-10-29 03:49:53 -0500

    Add files via upload

---
+++ Simple Game/html/src/com/badlogic/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.badlogic.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.drop.Drop;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(800, 400);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Drop();
+        }
+}
